dd1ee655ffa2aae869bf0439fed685cd6c7cba79
dmsgml93 <dmsgml6275@gmail.com>
2020-07-10 15:27:39 +0900

    2020. 07. 10 작성자 : 김은희 내   용 : Configuration.xml 추가

--- GRprj/src/main/java/com/min/gr/search/model/IService_Emp.java
+++ GRprj/src/main/java/com/min/gr/search/model/IService_Emp.java
@@ -1,5 +1,25 @@
 package com.min.gr.search.model;
 
+import java.util.List;
+
+import com.min.gr.emp.dto.EmpDTO;
+import com.min.gr.emp.dto.EmpUserDTO;
+import com.min.gr.ws.dto.WorkSpaceDTO;
+
 public interface IService_Emp {
 
+	/**
+	 * 사업장 조회
+	 * @return WS_NAME, WS_LOC, WS_NUM, WS_CODE
+	 */
+	public List<WorkSpaceDTO> wsSearch();
+	
+	/**
+	 * 입사신청(입사신청시 EMP_CODE, USER_ID, WS_CODE, EMP_CONFIRM, EMP_RESIGN이 자동으로 입력된다.)
+	 * @param dto
+	 * @return EMP_CODE, USER_ID, WS_CODE, EMP_CONFIRM, EMP_RESIGN
+	 */
+	public  boolean empApplyInsert(EmpDTO dto);
+	
+	public List<EmpUserDTO> empConfirmSelectD(String ws_code);
 }
