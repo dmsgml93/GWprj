385274b7e3db5c281b0ee5a68b65474d2523130f
dmsgml93 <dmsgml6275@gmail.com>
2020-07-12 21:43:58 +0900

    2020. 07. 12 작성자 : 김은희 내  용 : IService, Service, statement생성 및 수정

--- GRprj/src/main/java/com/min/gr/search/model/Service_Emp.java
+++ GRprj/src/main/java/com/min/gr/search/model/Service_Emp.java
@@ -1,5 +1,66 @@
 package com.min.gr.search.model;
 
+import java.util.List;
+import java.util.Map;
+
+import com.min.gr.emp.dto.EmpDTO;
+import com.min.gr.emp.dto.EmpUserDTO;
+import com.min.gr.ws.dto.WorkSpaceDTO;
+
 public class Service_Emp implements IService_Emp {
+
+	@Override
+	public List<WorkSpaceDTO> wsSearch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empApplyInsert(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<EmpUserDTO> empConfirmSelectD(String ws_code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empApplyUpdate(EmpDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<EmpUserDTO> empAllSelect(String ws_code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EmpUserDTO empDetailSelect(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empApplyReject(String emp_code) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean empUpdate(EmpDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean empdelete(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
